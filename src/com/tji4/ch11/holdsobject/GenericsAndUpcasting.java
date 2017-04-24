4e172e473888332f4638a11ef7a1b415849c0d33
Yi Fang <silver@Yis-MacBook-Pro.local>
2017-04-24 22:40:57 +0800

    add ch11

---
+++ src/com/tji4/ch11/holdsobject/GenericsAndUpcasting.java
@@ -0,0 +1,34 @@
+package com.tji4.ch11.holdsobject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by silver on 2017/4/24.
+ */
+
+class GrannySmith extends Apple{
+
+}
+
+class Gala extends Apple{}
+
+class Fuji extends Apple{}
+
+class Braeburn extends Apple{}
+public class GenericsAndUpcasting {
+
+    public static void main(String[] args){
+        ArrayList<Apple> apples = new ArrayList<Apple>();
+        apples.add(new GrannySmith());
+        apples.add(new Gala());
+        apples.add(new Fuji());
+        apples.add(new Braeburn());
+
+        for (Apple c:apples){
+            System.out.println(c);
+        }
+    }
+
+
+
+}
